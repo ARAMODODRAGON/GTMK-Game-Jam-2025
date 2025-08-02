395088edd907273be52bd9aff20ba61ce5770af4
CGSeanF <seanfowke@hotmail.com>
2025-08-01 20:51:05 -0500

    Fixed Damage Detection Issue
    -Added Timer Class
    -Fixed issue where the damage detection would miss due to a timing error, the timer class helps prevent this.

---
+++ GMTK2025/Assets/Scripts/Timer.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+	[Header("Timer Variables")]
+	//Inspector variables
+	[SerializeField]
+	bool isLooping = false;
+
+	[SerializeField]
+	float totalTime;
+
+	public float currentTime;
+
+	//Events
+	[Header("Events")]
+	public UnityEvent onTimerFinished;
+	public UnityEvent<float> onTimerUpdated;
+
+	//Private Variables
+	bool isRunning = false;
+
+
+	void Update()
+	{
+		if (isRunning)
+		{
+			currentTime -= Time.deltaTime;
+
+			onTimerUpdated.Invoke(currentTime);
+
+			if (currentTime <= 0)
+			{
+				isRunning = false;
+				onTimerFinished.Invoke();
+				if (isLooping)
+				{
+					StartTimer(totalTime);
+				}
+			}
+		}
+	}
+
+	public void StartTimer(float time_)
+	{
+		//Core assumption with this class is that we only fire the events once and then the object needs to subscribe again, unless we've specified looping.
+		//if (isLooping == false)
+		//{
+		//	onTimerFinished.RemoveAllListeners();
+		//	onTimerUpdated.RemoveAllListeners();
+		//}
+		totalTime = time_;
+		currentTime = time_;
+		isRunning = true;
+	}
+
+	public void StopTimer()
+	{
+		isRunning = false;
+		onTimerFinished.RemoveAllListeners();
+		onTimerUpdated.RemoveAllListeners();
+	}
+
+
+}
