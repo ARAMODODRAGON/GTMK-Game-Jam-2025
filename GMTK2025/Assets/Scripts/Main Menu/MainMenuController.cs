51d6b4309790c6935fb6c74a9a551c74005336f5
Domara Shlimon <domara2000@icloud.com>
2025-08-02 15:02:40 -0400

    Scrolling background

--- GMTK2025/Assets/Scripts/Main Menu/MainMenuController.cs
+++ GMTK2025/Assets/Scripts/Main Menu/MainMenuController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
 
@@ -11,12 +12,16 @@ public class MainMenuController : MonoBehaviour {
 	[SerializeField] private InputActionReference m_player2Direction;
 	[SerializeField] private InputActionReference m_player2Confirm;
 
+	// references
+	[SerializeField] private RawImage m_image;
+
 	// menus
 	[SerializeField] private List<BaseMenuScreen> m_menus;
 
 	// variables
 	private Stack<int> m_menuStack = new Stack<int>();
 	private BaseMenuScreen m_currentMenu => (m_menus.Count > 0) ? m_menus[m_menuStack.Peek()] : null;
+	private Vector2 m_imageSize;
 
 	private void Start() {
 		// sanity check
@@ -25,6 +30,10 @@ public class MainMenuController : MonoBehaviour {
 			enabled = false;
 			return;
 		}
+
+		// scrolling image
+		//m_image.uvRect = new Rect(0.0f, 0.0f, Screen.width, Screen.height);
+		m_imageSize = m_image.uvRect.max;
 
 		// initialize menus
 		for (int i = 0; i < m_menus.Count; i++) {
@@ -78,6 +87,11 @@ public class MainMenuController : MonoBehaviour {
 
 	private void Update() {
 		if (m_currentMenu) UpdateCurrentMenu();
+
+		Rect uv = m_image.uvRect;
+		uv.min += new Vector2(1.0f, 1.0f) * Time.deltaTime;
+		uv.max = uv.min + m_imageSize;
+		m_image.uvRect = uv;
 	}
 
 	private void UpdateCurrentMenu() {
