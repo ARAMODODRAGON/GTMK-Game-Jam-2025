62575272fd6e18af6a86e2d8fc87c88b10c63d9d
Domara Shlimon <domara2000@icloud.com>
2025-08-02 16:26:40 -0400

    Event bus created

---
+++ GMTK2025/Assets/Scripts/EventBus.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public static class EventBus {
+
+	// called to update the UI
+	// args (int playerindex, int health)
+	public static UnityEvent<int, int> updatePlayerHealth;
+
+	// called when the game ends
+	public static UnityEvent gameEnd;
+
+
+}
