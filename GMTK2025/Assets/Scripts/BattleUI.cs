2ca1953a66cad4ddc148b4d5487dd3c72276d0e9
Domara Shlimon <domara2000@icloud.com>
2025-08-02 17:03:44 -0400

    Work on battleUI

---
+++ GMTK2025/Assets/Scripts/BattleUI.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+public class BattleUI : MonoBehaviour {
+
+	// references
+	[SerializeField] private TextMeshPro m_player1HP;
+	[SerializeField] private TextMeshPro m_player2HP;
+	[SerializeField] private TextMeshPro m_timer;
+
+}
